8b30b5ad379a15b91095354d7d62c88881a7adf2
filipe4ndrade <58564278+filipe4ndrade@users.noreply.github.com>
2022-12-04 15:00:09 -0300

    Criando Algumas Propriedades Logicas

--- src/app/quizz/quizz.component.ts
+++ src/app/quizz/quizz.component.ts
@@ -6,6 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quizz.component.css']
 })
 export class QuizzComponent implements OnInit {
+  title:string = "";
+
+  //Dinâmica das Questões
+  questions:any;
+  questionSelected:any;
+
+  //Dinâmica das Respostas
+  answers:string[] =[];
+  answerSelected:string ="";
+
+  //Ponteiro que contabiliza as paginas
+  questionIndex:number = 0;
+  questionMaxIndex:number =0;
+
+  //Só mostrará o conteúdo da resposta quando finished for true
+  finished:boolean =false;
 
   constructor() { }
 
