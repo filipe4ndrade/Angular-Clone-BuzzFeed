76b429f0fd12ce565d8c3a8363454c4dcddf8a3d
filipe4ndrade <58564278+filipe4ndrade@users.noreply.github.com>
2022-12-04 15:36:44 -0300

    finalizando

--- src/app/quizz/quizz.component.ts
+++ src/app/quizz/quizz.component.ts
@@ -1,33 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import quizz_questions from "../assets/data/quizz.questions.json";
+import quizz_questions from '../assets/data/quizz.questions.json';
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
-  styleUrls: ['./quizz.component.css']
+  styleUrls: ['./quizz.component.css'],
 })
 export class QuizzComponent implements OnInit {
-  title:string = "";
+  title: string = '';
 
   //Dinâmica das Questões
-  questions:any;
-  questionSelected:any;
+  questions: any;
+  questionSelected: any;
 
   //Dinâmica das Respostas
-  answers:string[] =[];
-  answerSelected:string ="";
+  answers: string[] = [];
+  answerSelected: string = '';
 
   //Ponteiro que contabiliza as paginas
-  questionIndex:number = 0;
-  questionMaxIndex:number =0;
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
 
   //Só mostrará o conteúdo da resposta quando finished for true
-  finished:boolean =false;
+  finished: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    if(quizz_questions){
+    if (quizz_questions) {
       this.finished = false;
       this.title = quizz_questions.title;
 
@@ -36,42 +36,45 @@ export class QuizzComponent implements OnInit {
 
       this.questionIndex = 0;
       this.questionMaxIndex = this.questions.length;
-
-    }  
+    }
   }
 
   //Faz com que os valores das respostas sejam adicionados em um vetor answers
-  playerChoose(value:string){
+  playerChoose(value: string) {
     this.answers.push(value);
     this.nextStep();
   }
 
   //Pular pergunta
-  nextStep(){
-    this.questionIndex+=1;
+  async nextStep() {
+    this.questionIndex += 1;
 
-    if(this.questionMaxIndex > this.questionIndex){
+    if (this.questionMaxIndex > this.questionIndex) {
       this.questionSelected = this.questions[this.questionIndex];
-    }else{
+    } else {
+      const finalAnswer: string = await this.checkResult(this.answers);
       this.finished = true;
-      //verificar opção ganhadora 
+      this.answerSelected =
+        quizz_questions.results[
+          finalAnswer as keyof typeof quizz_questions.results
+        ];
+      //verificar opção ganhadora
     }
   }
 
   //verificar quem ganhou
-  async checkResult(anwsers:string[]){
-    const result = anwsers.reduce((previous, current,i,arr)=>{
-      if(arr.filter(item => item === previous).length>
-      arr.filter(item => item === current).length
-      ){
+  async checkResult(anwsers: string[]) {
+    const result = anwsers.reduce((previous, current, i, arr) => {
+      if (
+        arr.filter((item) => item === previous).length >
+        arr.filter((item) => item === current).length
+      ) {
         return previous;
-      }
-      else{
+      } else {
         return current;
       }
-    })
+    });
 
     return result;
   }
-
 }
