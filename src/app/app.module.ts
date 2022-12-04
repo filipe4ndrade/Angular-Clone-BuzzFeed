a68ffee218d4383e54730c10696adcccd019153e
filipe4ndrade <58564278+filipe4ndrade@users.noreply.github.com>
2022-12-04 14:48:57 -0300

    CSS e HTML basico criados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { QuizzComponent } from './quizz/quizz.component'
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent],
+  declarations: [ AppComponent, QuizzComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
